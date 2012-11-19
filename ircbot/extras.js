054b6b8a738559c9f3e59a05263399f85d373d1c
Brandon Benvie <brandon@bbenvie.com>
2012-11-19 07:45:36 -0500

    update ircbot extras

--- ircbot/extras.js
+++ ircbot/extras.js
@@ -1,5 +1,8 @@
-$__defineProps(this, {
-  reset(){
+Object.defineProperty(this, 'reset', {
+  configurable: true,
+  enumerable: false,
+  writable: true,
+  value(){
     $__Signal('reset');
   }
 });
