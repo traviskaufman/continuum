944078ad454ef7093b757365fb974afa18e19448
Brandon Benvie <brandon@bbenvie.com>
2012-11-29 01:05:46 -0500

    include escape, unescape, and typed arrays in the default lineup

--- modules/@std.js
+++ modules/@std.js
@@ -4,7 +4,8 @@ const Infinity  = 1 / 0;
 const undefined = void 0;
 
 // standard functions
-import { decodeURI,
+import { escape,
+         decodeURI,
          decodeURIComponent,
          encodeURI,
          encodeURIComponent,
@@ -12,7 +13,8 @@ import { decodeURI,
          isFinite,
          isNaN,
          parseFloat,
-         parseInt } from '@globals';
+         parseInt,
+         unescape } from '@globals';
 
 
 import { clearInterval,
@@ -45,6 +47,16 @@ import { Error,
          TypeError,
          URIError } from '@error';
 
+import { ArrayBuffer,
+         DataView,
+         Float32Array,
+         Float64Array,
+         Int16Array,
+         Int32Array,
+         Int8Array,
+         Uint16Array,
+         Uint32Array,
+         Uint8Array } from '@typed-arrays';
 
 // standard pseudo-modules
 import JSON from '@json';
@@ -58,10 +70,12 @@ import console from '@console';
 let StopIteration = $__StopIteration
 
 
+
 export Array, Boolean, Date, Function, Map, Number, Object, Proxy, RegExp, Set, String, WeakMap,
        Error, EvalError, RangeError, ReferenceError, SyntaxError, TypeError, URIError,
-       decodeURI, decodeURIComponent, encodeURI, encodeURIComponent, eval, isFinite, isNaN,
-       parseFloat, parseInt, clearInterval, clearTimeout, setInterval, setTimeout,
-       console, StopIteration, JSON, Math, NaN, Infinity, undefined;
-
-
+       ArrayBuffer, DataView, Float32Array, Float64Array, Int16Array,
+       Int32Array, Int8Array, Uint16Array, Uint32Array, Uint8Array,
+       clearInterval, clearTimeout, decodeURI, decodeURIComponent, escape, encodeURI, encodeURIComponent,
+       eval, isFinite, isNaN, parseFloat, parseInt, setInterval, setTimeout, unescape,
+       console, StopIteration, JSON, Math,
+       NaN, Infinity, undefined;
