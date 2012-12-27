345974264fbad01f7e9cd201bc4a03ca712e2772
Brandon Benvie <brandon@bbenvie.com>
2012-12-27 15:04:36 -0500

    fix the various functions in @iter

--- engine/builtins/@iter.js
+++ engine/builtins/@iter.js
@@ -10,84 +10,78 @@ builtinClass(Iterator);
 
 
 export function keys(obj){
-  return {
-    @iterator: ()=> (function*(){
-      for (let x in obj) {
-        if (obj.@@has(x)) {
-          yield x;
-        }
+  obj = $__ToObject(obj);
+  return (function*(){
+    for (let x in obj) {
+      if (obj.@@has(x)) {
+        yield x;
       }
-    })()
-  };
+    }
+  })();
 }
 
 builtinFunction(keys);
 
 
 export function values(obj){
-  return {
-    @iterator: ()=> (function*(){
-      for (let x in obj) {
-        if (obj.@@has(x)) {
-          yield obj[x];
-        }
+  obj = $__ToObject(obj);
+  return (function*(){
+    for (let x in obj) {
+      if (obj.@@has(x)) {
+        yield obj[x];
       }
-    })()
-  };
+    }
+  })();
 }
 
 builtinFunction(values);
 
 
 export function entries(obj){
-  return {
-    @iterator: ()=> (function*(){
-      for (let x in obj) {
-        if (obj.@@has(x)) {
-          yield [x, obj[x]];
-        }
+  obj = $__ToObject(obj);
+  return (function*(){
+    for (let x in obj) {
+      if (obj.@@has(x)) {
+        yield [x, obj[x]];
       }
-    })()
-  };
+    }
+  })();
 }
 
 builtinFunction(entries);
 
 
 export function allKeys(obj){
-  return {
-    @iterator: ()=> (function*(){
-      for (let x in obj) {
-        yield x;
-      }
-    })()
-  };
+  obj = $__ToObject(obj);
+  return (function*(){
+    for (let x in obj) {
+      yield x;
+    }
+  })();
 }
 
 builtinFunction(allKeys);
 
 
 export function allValues(obj){
-  return {
-    @iterator: ()=> (function*(){
-      for (let x in obj) {
-        yield obj[x];
-      }
-    })()
-  };
+  obj = $__ToObject(obj);
+  return (function*(){
+    for (let x in obj) {
+      yield obj[x];
+    }
+  })();
 }
 
 builtinFunction(allValues);
 
 
 export function allEntries(obj){
-  return {
-    @iterator: ()=> (function*(){
-      for (let x in obj) {
-        yield [x, obj[x]];
-      }
-    })()
-  };
+  obj = $__ToObject(obj);
+  return (function*(){
+    for (let x in obj) {
+      yield [x, obj[x]];
+    }
+  })();
 }
 
 builtinFunction(allEntries);
