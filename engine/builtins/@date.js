8c95ef0a5d95b38440b7b4c16a8b1333ccb03e34
Brandon Benvie <brandon@bbenvie.com>
2013-01-05 10:47:25 -0500

    adjust the Date.parse regexp

--- engine/builtins/@date.js
+++ engine/builtins/@date.js
@@ -527,7 +527,7 @@ function toLocaleTimeString(t){
 // ### 15.9.4.2 Date.parse ###
 // ###########################
 
-const dateParser = /^(\d{4}|(?:[+\-]\d{6}))(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/;
+const dateParser = /^(\d{4}|[+\-]\d{6})(?:-(\d\d)(?:-(\d\d))?)?(?:T(\d\d):(\d\d)(?::(\d\d)(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d\d)(?::(\d\d))?)?)?$/;
 
 export function parse(date){
   const match = $$RegExpExec(dateParser, ToString(date));
