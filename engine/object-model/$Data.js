8cdbe75ff260b822ab614d4eaeb72ab69bddb082
Brandon Benvie <brandon@bbenvie.com>
2013-01-07 15:58:55 -0500

    bit more work on binary data

--- engine/object-model/$Data.js
+++ engine/object-model/$Data.js
@@ -8,61 +8,22 @@ var $Data = (function(exports){
     Value: undefined,
     DataType: undefined
   },[
+  ]);
+
+
+  function $DataType(){
+
+  }
+
+  define($DataType.prototype, }
+    BuiltinBrand: 'BuiltinDataType',
+    DataType: undefined
+  },[
     function Convert(){},
     function Reify(){},
     function IsSame(){}
   ]);
 
+
   return exports;
 })(typeof module !== 'undefined' ? exports : {});
-
-/*
-t = new ArrayType(elementType, length)
-
-t = new StructType(fields)
-
-let S = new StructType({
-  i: int32,
-  j: int32,
-  o: ObjectPointer,
-  s: StringPointer
-});
-
-
-let x = new S;
-x.o // null
-x.s // null
-x.o = document;
-x.o === document // true
-
-ObjectPointer
-StringPointer
-
-
-const S = new StructType(...);
-const A = new ArrayType(S);
-
-let a = new A(1000000);
-let p = new StructView(S);
-
-for (let i = 0; i < a.length; i++) {
-  p.setView(a, i);
-  console.log(p.x + ", " + p.y);
-}
-
-
-
-let S = new StructType({
-  foo: new StructType({
-    bar: int32,
-  }),
-});
-let A = new ArrayType(S);
-
-let a = new A(1000000);
-let total = 0;
-
-for (let bar of a.cursor("foo", "bar")) {
-  total += bar;
-}
-*/
