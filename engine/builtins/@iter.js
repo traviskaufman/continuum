7b6419c29f9db770e09371486ab49865dfa710df
Brandon Benvie <brandon@bbenvie.com>
2013-01-07 16:57:22 -0500

    update to latest changes on the standard library es-wiki page by adding @dict and changing iter to have zip and unzip

--- engine/builtins/@iter.js
+++ engine/builtins/@iter.js
@@ -1,4 +1,57 @@
+import {
+  builtinClass,
+  builtinFunction
+} from '@@utilities';
+
+import {
+  @@iterator: iterator
+} from '@@symbols';
+
+import {
+  ToObject
+} from '@@operations';
+
+
 export const iterator = @@iterator;
+
+
+export function zip(a, b){
+  const keys = a.@@iterator(),
+        vals = b.@@iterator();
+
+  return (function*(){
+    while (true) {
+      yield [keys.next(), vals.next()];
+    }
+  })();
+}
+
+builtinFunction(zip);
+
+
+export function unzip(iterable){
+  const obj = ToObject(iterable);
+
+  if ('values' in obj) {
+    const keys = 'keys' in obj ? obj.keys() : obj.values(),
+          vals = obj.values();
+
+    return [ (function*(){ while (1) yield keys.next() })(),
+             (function*(){ while (1) yield vals.next() })() ];
+  }
+
+  let key = 'entries' in obj ? 'entries' : @@iterator in obj ? @@iterator : null;
+  if (key) {
+    const keys = obj[key](),
+          vals = obj[key]();
+
+    return [ (function*(){ while (1) yield keys.next()[0] })(),
+             (function*(){ while (1) yield vals.next()[1] })() ];
+  }
+}
+
+builtinFunction(unzip);
+
 
 export class Iterator {
   @@iterator(){
@@ -7,81 +60,3 @@ export class Iterator {
 }
 
 builtinClass(Iterator);
-
-
-export function keys(obj){
-  obj = $__ToObject(obj);
-  return (function*(){
-    for (let x in obj) {
-      if ($__has(obj, x)) {
-        yield x;
-      }
-    }
-  })();
-}
-
-builtinFunction(keys);
-
-
-export function values(obj){
-  obj = $__ToObject(obj);
-  return (function*(){
-    for (let x in obj) {
-      if ($__has(obj, x)) {
-        yield obj[x];
-      }
-    }
-  })();
-}
-
-builtinFunction(values);
-
-
-export function entries(obj){
-  obj = $__ToObject(obj);
-  return (function*(){
-    for (let x in obj) {
-      if ($__has(obj, x)) {
-        yield [x, obj[x]];
-      }
-    }
-  })();
-}
-
-builtinFunction(entries);
-
-
-export function allKeys(obj){
-  obj = $__ToObject(obj);
-  return (function*(){
-    for (let x in obj) {
-      yield x;
-    }
-  })();
-}
-
-builtinFunction(allKeys);
-
-
-export function allValues(obj){
-  obj = $__ToObject(obj);
-  return (function*(){
-    for (let x in obj) {
-      yield obj[x];
-    }
-  })();
-}
-
-builtinFunction(allValues);
-
-
-export function allEntries(obj){
-  obj = $__ToObject(obj);
-  return (function*(){
-    for (let x in obj) {
-      yield [x, obj[x]];
-    }
-  })();
-}
-
-builtinFunction(allEntries);
