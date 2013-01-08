0661fb57f63d8106c40c534ba22323f4a2def8f4
Brandon Benvie <brandon@bbenvie.com>
2013-01-08 14:39:16 -0500

    when used in node, automatically hook up the console when creating a new realm

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 var continuum = module.exports = require('./engine');
+
+continuum.createRealm = function createRealm(callback){
+  return new continuum.Realm(callback).useConsole(console);
+};
+
+continuum.createRealmAsync = function createRealmAsync(){
+  return new continuum.Realm(true).useConsole(console);
+};
