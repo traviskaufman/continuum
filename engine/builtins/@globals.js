5660fb051f37641c7dae99b0d85ce566448c835b
Brandon Benvie <brandon@bbenvie.com>
2013-01-08 16:41:12 -0500

    update @globals to new conventions

--- engine/builtins/@globals.js
+++ engine/builtins/@globals.js
@@ -1,37 +1,56 @@
-$__define($__global, @@toStringTag, 'global');
-$__define($__StopIteration, @@toStringTag, 'StopIteration');
+import {
+  $$Get,
+  $$Set
+} from '@@internals';
+
+import {
+  define
+} from '@@utilities';
+
+import {
+  ToNumber,
+  ToPrimitive,
+  ToString
+} from '@@operations';
+
+import {
+  @@toStringTag: toStringTag
+} from '@@symbols';
+
+define($__global, @@toStringTag, 'global');
+define($__StopIteration, @@toStringTag, 'StopIteration');
 
 
 export function decodeURI(value){
-  return $__decodeURI($__ToString(value));
+  return $__decodeURI(ToString(value));
 }
 
 builtinFunction(decodeURI);
 
 
 export function decodeURIComponent(value){
-  return $__decodeURIComponent($__ToString(value));
+  return $__decodeURIComponent(ToString(value));
 }
 
 builtinFunction(decodeURIComponent);
 
 
 export function encodeURI(value){
-  return $__encodeURI($__ToString(value));
+  return $__encodeURI(ToString(value));
 }
 
 builtinFunction(encodeURI);
 
 
 export function encodeURIComponent(value){
-  return $__encodeURIComponent($__ToString(value));
+  return $__encodeURIComponent(ToString(value));
 }
 
 builtinFunction(encodeURIComponent);
 
 
 export function escape(value){
-  return $__escape($__ToString(value));
+  return $__escape(ToString(value));
 }
 
 builtinFunction(escape);
@@ -39,12 +58,12 @@ builtinFunction(escape);
 
 export function eval(source){}
 builtinFunction(eval);
-$__setInternal(eval, 'Call', $__getInternal($__eval, 'Call'));
-$__setInternal(eval, 'Construct', $__getInternal($__eval, 'Construct'));
+$$Set(eval, 'Call', $$Get($__eval, 'Call'));
+$$Set(eval, 'Construct', $$Get($__eval, 'Construct'));
 
 
 export function isFinite(number){
-  number = $__ToNumber(number);
+  number = ToNumber(number);
   return number === number && number !== Infinity && number !== -Infinity;
 }
 
@@ -52,7 +71,7 @@ builtinFunction(isFinite);
 
 
 export function isNaN(number){
-  number = $__ToNumber(number);
+  number = ToNumber(number);
   return number !== number;
 }
 
@@ -60,21 +79,21 @@ builtinFunction(isNaN);
 
 
 export function parseFloat(value){
-  return $__parseFloat($__ToPrimitive(value));
+  return $__parseFloat(ToPrimitive(value));
 }
 
 builtinFunction(parseFloat);
 
 
 export function parseInt(value, radix){
-  return $__parseInt($__ToPrimitive(value), +radix);
+  return $__parseInt(ToPrimitive(value), +radix);
 }
 
 builtinFunction(parseInt);
 
 
 export function unescape(value){
-  return $__unescape($__ToString(value));
+  return $__unescape(ToString(value));
 }
 
 builtinFunction(unescape);
