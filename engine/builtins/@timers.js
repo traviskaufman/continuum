69f39bbd4505d057c97d6c75b073ad127da99611
Brandon Benvie <brandon@bbenvie.com>
2013-01-19 00:02:50 -0500

    DRY @timers

--- engine/builtins/@timers.js
+++ engine/builtins/@timers.js
@@ -4,7 +4,8 @@ import {
 } from '@@operations';
 
 import {
-  builtinFunction
+  builtinFunction,
+  internalFunction
 } from '@@utilities';
 
 import {
@@ -17,6 +18,13 @@ import {
 import {
   Function
 } from '@function';
+
+
+function prepareCallback(callback){
+  return typeof callback === 'function' ? callback : new Function(ToString(callback));
+}
+
+internalFunction(prepareCallback);
 
 
 export function clearImmediate(id){
@@ -41,33 +49,21 @@ builtinFunction(clearTimeout);
 
 
 export function setImmediate(callback){
-  if (typeof callback !== 'function') {
-    callback = new Function(ToString(callback));
-  }
-
-  return $$SetImmediate(callback);
+  return $$SetImmediate(prepareCallback(callback));
 }
 
 builtinFunction(setImmediate);
 
 
 export function setInterval(callback, milliseconds){
-  if (typeof callback !== 'function') {
-    callback = new Function(ToString(callback));
-  }
-
-  return $$SetTimer(callback, ToInteger(milliseconds), true);
+  return $$SetTimer(prepareCallback(callback), ToInteger(milliseconds), true);
 }
 
 builtinFunction(setInterval);
 
 
 export function setTimeout(callback, milliseconds){
-  if (typeof callback !== 'function') {
-    callback = new Function(ToString(callback));
-  }
-
-  return $$SetTimer(callback, ToInteger(milliseconds), false);
+  return $$SetTimer(prepareCallback(callback), ToInteger(milliseconds), false);
 }
 
 builtinFunction(setTimeout);
