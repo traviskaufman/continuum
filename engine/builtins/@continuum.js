f88171e2a7926b22817a0fc216f84a6875c435b2
Brandon Benvie <brandon@bbenvie.com>
2013-01-23 13:06:55 -0800

    Remove defunct exports from @continuum

--- engine/builtins/@continuum.js
+++ engine/builtins/@continuum.js
@@ -1,8 +1,4 @@
 export const createUndetectable = $__createUndetectable,
              unwrapUndetectable = $__unwrapUndetectable,
              isUndetectable     = $__isUndetectable,
-             callerName         = $__callerName,
-             isConstruct        = $__isConstruct,
-             argumentCount      = $__argumentCount,
-             hasArgument        = $__hasArgument,
              nil                = $__createNil();
