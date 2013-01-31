a1ca6d7012533312acd81dd6d0fbc33fc49048f2
Brandon Benvie <brandon@bbenvie.com>
2013-01-30 20:36:03 -0500

    use ToBoolean instead of !!

--- engine/builtins/@symbol.js
+++ engine/builtins/@symbol.js
@@ -7,13 +7,17 @@ import {
   builtinClass
 } from '@@utilities';
 
+import {
+  ToBoolean
+} from '@@operations';
+
 
 export class Symbol {
   constructor(name, isPublic){
     if (name == null) {
       throw $$Exception('unnamed_symbol', []);
     }
-    return $$CreateObject('Symbol', name, !!isPublic);
+    return $$CreateObject('Symbol', name, ToBoolean(isPublic));
   }
 }
 
