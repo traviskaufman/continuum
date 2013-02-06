b00356df3d54d1ed024f4bf32f58e2459336938f
Brandon Benvie <brandon@bbenvie.com>
2013-02-05 23:01:44 -0500

    partially rework @map and begin annotating it

--- engine/builtins/@map.js
+++ engine/builtins/@map.js
@@ -8,17 +8,18 @@ import {
 } from '@@constants';
 
 import {
+  $$CallerName,
   $$Exception,
   $$Get,
   $$Has,
   $$Set
 } from '@@internals';
 
-
 import {
   builtinClass,
   builtinFunction,
   define,
+  ensureObject,
   extend,
   hasBrand,
   internalFunction,
@@ -41,6 +42,7 @@ import {
   $$MapDelete,
   $$MapGet,
   $$MapHas,
+  // 15.14.1.1 MapInitialisation (TODO: self-host in ES6)
   $$MapInitialization,
   $$MapNext,
   $$MapSet,
@@ -57,13 +59,16 @@ import {
 } from '@reflect';
 
 
-function ensureMap(o, name){
+const sigil = $$MapSigil();
+
+function ensureMap(o){
   if (Type(o) !== 'Object' || !$$Has(o, 'MapData')) {
-    throw $$Exception('called_on_incompatible_object', [name]);
+    throw $$Exception('called_on_incompatible_object', [$$CallerName()]);
   }
 }
 
 internalFunction(ensureMap);
+
 
 
 class MapIterator extends Iterator {
@@ -73,21 +78,19 @@ class MapIterator extends Iterator {
 
   constructor(map, kind){
     this.@map = ToObject(map);
-    this.@key = $$MapSigil();
+    this.@key = sigil;
     this.@kind = kind;
   }
 
   next(){
-    if (Type(this) !== 'Object') {
-      throw $$Exception('called_on_non_object', ['MapIterator.prototype.next']);
-    }
+    ensureObject(this);
 
     if (!(hasOwn(this, @map) && hasOwn(this, @key) && hasOwn(this, @kind))) {
       throw $$Exception('called_on_incompatible_object', ['MapIterator.prototype.next']);
     }
 
-    var kind = this.@kind,
-        item = $$MapNext(this.@map, this.@key);
+    const kind = this.@kind,
+          item = $$MapNext(this.@map, this.@key);
 
     if (!item) {
       throw StopIteration;
@@ -100,6 +103,7 @@ class MapIterator extends Iterator {
     } else if (kind === 'key') {
       return item[0];
     }
+
     return item[1];
   }
 }
@@ -107,61 +111,92 @@ class MapIterator extends Iterator {
 builtinClass(MapIterator);
 
 
+// ######################################################
+// ### 15.14.5 Properties of the Map Prototype Object ###
+// ######################################################
+
 export class Map {
+  // #######################################
+  // # 15.14.5.1 Map.prototype.constructor #
+  // #######################################
   constructor(iterable){
     if (!isInitializing(this, 'MapData')) {
+      // 15.14.2 The Map Constructor Called as a Function
       return new Map(iterable);
     }
 
+    // 15.14.3 The Map Constructor
     $$MapInitialization(this, iterable);
+  }
+
+  // #################################
+  // # 15.14.5.2 Map.prototype.clear #
+  // #################################
+  clear(){
+    ensureMap(this);
+    $$MapClear(this, key);
+    return this;
+  }
+
+  // ##################################
+  // # 15.14.5.3 Map.prototype.delete #
+  // ##################################
+  delete(key){
+    ensureMap(this);
+    return $$MapDelete(this, key);
+  }
+
+  // ###################################
+  // # 15.14.5.4 Map.prototype.forEach #
+  // ###################################
+  forEach(callbackfn, thisArg = undefined){
+    ensureMap(this);
+    ensureCallback(callbackfn);
+
+    let item = $$MapNext(this, sigil);
+
+    while (item) {
+      call(callbackfn, this, item[1], item[0], this);
+      item = $$MapNext(this, item);
+    }
   }
 
   get size(){
     if (this && $$Has(this, 'MapData')) {
       return $$MapSize(this);
     }
+
     return 0;
   }
 
-  clear(){
-    ensureMap(this, 'Map.prototype.clear');
-    $$MapClear(this, key);
-    return this;
-  }
-
-  delete(key){
-    ensureMap(this, 'Map.prototype.delete');
-    return $$MapDelete(this, key);
-  }
-
   get(key){
-    ensureMap(this, 'Map.prototype.get');
+    ensureMap(this);
     return $$MapGet(this, key);
   }
 
   has(key){
-    ensureMap(this, 'Map.prototype.has');
+    ensureMap(this);
     return $$MapHas(this, key);
   }
 
   entries(){
-    ensureMap(this, 'Map.prototype.entries');
+    ensureMap(this);
     return new MapIterator(this, 'key+value');
   }
 
   keys(){
-    ensureMap(this, 'Map.prototype.keys');
+    ensureMap(this);
     return new MapIterator(this, 'key');
   }
 
   set(key, value){
-    ensureMap(this, 'Map.prototype.set');
+    ensureMap(this);
     $$MapSet(this, key, value);
     return this;
   }
 
   values(){
-    ensureMap(this, 'Map.prototype.values');
+    ensureMap(this);
     return new MapIterator(this, 'value');
   }
 }
@@ -175,74 +210,5 @@ extend(Map, {
   }
 });
 
+define(Map.prototype, @@iterator, Map.prototype.entries);
 builtinClass(Map);
-const MapPrototype = Map.prototype;
-define(MapPrototype, @@iterator, MapPrototype.entries);
-
-
-
-function mapClear(map){
-  ensureMap(map, '@map.clear');
-  $$MapClear(map);
-  return map;
-}
-
-builtinFunction(mapClear);
-
-
-function mapDelete(map, key){
-  ensureMap(map, '@map.delete');
-  return $$MapDelete(map, key);
-}
-
-builtinFunction(mapDelete);
-
-
-function mapGet(map, key){
-  ensureMap(map, '@map.get');
-  return $$MapGet(map, key);
-}
-
-builtinFunction(mapGet);
-
-
-function mapHas(map, key){
-  ensureMap(map, '@map.has');
-  return $$MapHas(map, key);
-}
-
-builtinFunction(mapHas);
-
-
-function mapIterate(map, kind){
-  ensureMap(map, '@map.iterate');
-  return new MapIterator(map, kind === undefined ? 'key+value' : ToString(kind));
-}
-
-builtinFunction(mapIterate);
-
-
-function mapSet(map, key, value){
-  ensureMap(map, '@map.set');
-  $$MapSet(map, key, value);
-  return map;
-}
-
-builtinFunction(mapSet);
-
-
-function mapSize(map){
-  ensureMap(map, '@map.size');
-  return $$MapSize(map);
-}
-
-builtinFunction(mapSize);
-
-
-export const clear   = mapClear,
-           //delete  = mapDelete, TODO: fix exporting reserved names
-             get     = mapGet,
-             has     = mapHas,
-             iterate = mapIterate,
-             set     = mapSet,
-             size    = mapSize;
